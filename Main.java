e6e3ad451925077e1a325863fb623a3ace9203b1
MegaDen58 <96517069+MegaDen58@users.noreply.github.com>
2022-04-05 17:28:35 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,15 +3,6 @@ import java.util.Scanner;
 
 
 public class Main {
-
-    public static int factorial(int x){
-        int result = 1;
-        for(int i = 1; i <= x; i++){
-            result *= i;
-        }
-        return result;
-    }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите число x: ");
